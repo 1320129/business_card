9ae4f42dab20df50250782639bcc6e2000994e56
wo_njh <showgur3000@gmail.com>
2021-06-07 17:51:36 +0900

    business_card

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import styles from './card.module.css';
 import Button from '../button/button';
+import ImageFileInput from '../image_file_input/image_file_input';
 
 
 const Card = ({card}) => {
     const onClick = () => {
-        
+
     }
     return(
         <div className={styles.card_make}>
             <h2>Card Maker</h2>
             
-            <div className={styles.card_list}>
+            <div className={styles.card_make_list}>
                
                     {card.map(card => {
                         return (
-                        <form className="card">
-                            <input type="text" value={card.name}/>
-                            <input type="text" value={card.company}/>
-                            <input type="text" value={card.color}/>
-                            <input type="text" value={card.position}/>
-                            <input type="text" value={card.email}/>
-                            <textarea type="text" value={card.description}/>
+                        <form className={styles.card}>
+                            <input type="text" name="name" value={card.name} className={styles.col_3}/>
+                            <input type="text" name="company" value={card.company} className={styles.col_3}/>
+                            <select name="color" value={card.color} className={styles.col_3}>
+                                <option value="Light">Light</option>
+                                <option value="Red">Red</option>
+                                <option value="Blue">Blue</option>
+                            </select>
+                            <input type="text" name="position" value={card.position} className={styles.col_2}/>
+                            <input type="text" name="email" value={card.email} className={styles.col_2}/>
+                            <textarea type="text"name="description" value={card.description} className={styles.col_1}/>
+                            <ImageFileInput />
                             <Button name={'delete'}onClick={onClick}/>
                         </form>
                         
